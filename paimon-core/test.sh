aa8896adaaa50f5f68027368d9c04e8ceedf7281
Aitozi <aitozi@apache.org>
2024-04-07 19:45:25 +0800

    remove

--- paimon-core/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# source ~/.zshrc
-# 循环执行10次
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
-for i in {1..10}; do
-    mvn -s ~/.m2/settings-central.xml test -Dtest=org.apache.paimon.operation.OrphanFilesCleanTest
-done
